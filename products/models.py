18f507f9b54c5082d48a6e07f52230b060f1e348
mabin9527 <ma.bin9527000@gmail.com>
2023-07-19 17:44:23 +0000

    Created products app and models

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+    db_table = 'bookstore_category'
+    cid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=254)
+
+
+class Product(models.Model):
+    db_table = 'bookstore_product'
+    pid = models.AutoField(primary_key=True)
+    category = models.ForeignKey(
+        Category, related_name='category_product', on_delete=models.CASCADE
+        )
+    pdname = models.CharField(max_length=254)
+    pdprice = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.FloatField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    description = models.TextField()
