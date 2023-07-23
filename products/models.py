d3803bcdee51ed86596d4cb67b96df1f54d259a0
mabin9527 <ma.bin9527000@gmail.com>
2023-07-23 18:35:33 +0000

    Updated models and loaded fixture data to products app

--- products/models.py
+++ products/models.py
@@ -4,7 +4,6 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
     cid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(
@@ -19,17 +18,18 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(
-        'Category', related_name='category_product', null=True, blank=True,
-        on_delete=models.SET_NULL
-        )
+
     pid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=254)
     author = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    discount = models.FloatField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(
+    'Category', related_name='category_product', null=True, blank=True,
+    on_delete=models.SET_NULL
+    )
+    image = models.ImageField(max_length=1024, null=True, blank=True)
+    
 
     def __str__(self):
         return self.name
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.products, name='products_page'), 
