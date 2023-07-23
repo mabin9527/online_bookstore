0c7f5852f9b19400bafe89ab17604dccd377dcc6
mabin9527 <ma.bin9527000@gmail.com>
2023-07-23 18:56:19 +0000

    Added products function to products.views.py file

--- products/views.py
+++ products/views.py
@@ -6,10 +6,10 @@ def products(request):
     A view to show the main page of bookstore, including book categorys,
     products and search queries
     """
-    # categorys = Category.objects.all()
-    # books = Product.objects.filter().all()
-    # context = {
-    #     'categorys': categorys,
-    #     'books': books
-    # }
-    return render(request, 'products/products.html')
+    categorys = Category.objects.all()
+    books = Product.objects.filter().all()
+    context = {
+        'categorys': categorys,
+        'books': books
+    }
+    return render(request, 'products/products.html', context)
