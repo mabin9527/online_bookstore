94a35fdbfd666dc20a9599e40de040729336e6e9
mabin9527 <ma.bin9527000@gmail.com>
2023-07-23 08:36:38 +0000

    Migrate changes to databases

--- products/models.py
+++ products/models.py
@@ -1,22 +1,35 @@
 from django.db import models
 
 class Category(models.Model):
-    db_table = 'bookstore_category'
-    cid = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=254)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
+    cid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254, null=True, blank=True
+        )
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 class Product(models.Model):
-    db_table = 'bookstore_product'
-    pid = models.AutoField(primary_key=True)
     category = models.ForeignKey(
-        Category, related_name='category_product', on_delete=models.CASCADE
+        'Category', related_name='category_product', null=True, blank=True,
+        on_delete=models.SET_NULL
         )
-    pdname = models.CharField(max_length=254)
-    pdprice = models.DecimalField(max_digits=6, decimal_places=2)
+    pid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=254)
+    author = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     discount = models.FloatField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
