3a229181cb86f0173b6095d5595fd76dad88ce71
mabin9527 <ma.bin9527000@gmail.com>
2023-07-25 09:29:50 +0000

    Added product detail view function to views and urls to urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.products, name='products_page'), 
+    path('detail/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -13,3 +13,22 @@ def products(request):
         'books': books
     }
     return render(request, 'products/products.html', context)
+
+def product_detail(request):
+    """
+    View to show individual product details
+    """
+    pid = request.GET.get('pid')
+    product = Product.objects.filter(pid=pid).first()
+    category = Category.objects.filter(cid=product.category_id).first()
+    img_list = Product.image.split('##')
+    price = Product.price
+    books = Product.objects.filter(category_id=product.category_id)[0:3]
+    context = {
+        'product': product,
+        'category': category,
+        'img_list': img_list,
+        'price': price,
+        'books': books,
+    }
+    return render(request, 'products/product_detail.html', context)
