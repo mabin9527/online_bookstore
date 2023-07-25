58dd8f6b245e1c593001c258dfa6292078b5d764
mabin9527 <ma.bin9527000@gmail.com>
2023-07-25 18:59:39 +0000

    Added breadcrumb to product_detail template

--- products/views.py
+++ products/views.py
@@ -18,17 +18,16 @@ def product_detail(request):
     """
     View to show individual product details
     """
-    pid = request.GET.get('pid')
-    product = Product.objects.filter(pid=pid).first()
-    category = Category.objects.filter(cid=product.category_id).first()
-    img_list = Product.image.split('##')
-    price = Product.price
-    books = Product.objects.filter(category_id=product.category_id)[0:3]
-    context = {
-        'product': product,
-        'category': category,
-        'img_list': img_list,
-        'price': price,
-        'books': books,
-    }
-    return render(request, 'products/product_detail.html', context)
+    # pid = request.GET.get('pid')
+    # product = Product.objects.filter(pid=pid).first()
+    # print(product.category_id)
+    # category = Category.objects.filter(cid=product.category_id).first()
+    # price = Product.price
+    # books = Product.objects.filter(cid=product.category_id)[0:3]
+    # context = {
+    #     'product': product,
+    #     'category': category,
+    #     'price': price,
+    #     'books': books,
+    # }
+    return render(request, 'products/product_detail.html')
