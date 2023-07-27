bba6ac770222fda0e65e1a23c727ea8ee47bb21e
mabin9527 <ma.bin9527000@gmail.com>
2023-07-27 07:44:13 +0000

    Added form action url to base.html and mobile-top-header.html

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.products, name='products_page'), 
     path('detail/', views.product_detail, name='product_detail'),
+    path('add/', views.add_to_cart, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product, Category
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import Product, Category
+from .forms import ProductForm
 
 def products(request):
     """
@@ -26,24 +30,24 @@ def product_detail(request):
     }
     return render(request, 'products/product_detail.html', context)
 
-@login_required
-def add_to_cart(request):
-    """
-    Add selected product to the shopping bag
-    """
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save()
-            messages.success(request, 'The item has been successfully added to your basket!')
-            return redirect(reverse('product_detail', args=[product.id]))
-        else:
-            messages.error(request, 'Failed to add product. Please check form is valid.')
-    else:
-        form = ProductForm()
+# @login_required
+# def add_to_cart(request):
+#     """
+#     Add selected product to the shopping bag
+#     """
+#     if request.method == "POST":
+#         form = ProductForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             product = form.save()
+#             messages.success(request, 'The item has been successfully added to your basket!')
+#             return redirect(reverse('product_detail', args=[product.id]))
+#         else:
+#             messages.error(request, 'Failed to add product. Please check form is valid.')
+#     else:
+#         form = ProductForm()
     
-    template = 'products/add_product.html'
-    context = {
-        'form': form,
-    }
-    return render(request, template, context)
+#     template = 'products/add_product.html'
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, template, context)
