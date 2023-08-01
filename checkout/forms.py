e40975c6122bd52081d632dcde43e0ef9771b403
mabin9527 <ma.bin9527000@gmail.com>
2023-08-01 08:01:09 +0000

    Added form to checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+        'full_name', 'email_address', 'mobile_number', 
+        'street_address1', 'street_address2',
+        'city', 'county', 'postcode', 'country',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated labels 
+        and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email_address': 'Email Address',
+            'mobile_number': 'Mobile Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'city': 'City',
+            'county': 'County',
+            'postcode': 'Eir Code',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].widget.attrs['aria-label'] = placeholders[
+                    field]
+                self.fields[field].label = False
