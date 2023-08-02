4cfb16414897f9eecb426a1862da9b08554683a8
mabin9527 <ma.bin9527000@gmail.com>
2023-08-02 16:51:16 +0000

    Added order history views and templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
+from products.models import Product, Category
 
 
 
@@ -10,14 +12,59 @@ def profile(request):
     """
     Display user profile
     """
+    categories_list = Category.objects.all()
     profile = get_object_or_404(
-        UserProfile,user=request.user
-    )
-    form = UserProfileForm(instance=profile)
+        UserProfile,
+        user=request.user
+        )
+
+    if request.method == 'POST':
+        form = UserProfileForm(
+            request.POST,
+            instance=profile
+            )
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Profile updated successfully'
+                )
+        else:
+            messages.error(
+                request, 'Update failed. Please check the form is valid.'
+                )
+    else:
+        form = UserProfileForm(instance=profile)
+
     orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
+        'categories_list': categories_list
     }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    categories_list = Category.objects.all()
+    order = get_object_or_404(
+        Order,
+        order_number=order_number
+        )
+
+    messages.info(request, (
+        f'This is a previous confirmation for order number {order_number}. '
+        'Confirmation email sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+        'categories_list': categories_list,
+    }
+
     return render(request, template, context)
