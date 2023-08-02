144cf3e63c1799942b1a27a7b4bffc33355063bd
mabin9527 <ma.bin9527000@gmail.com>
2023-08-02 18:21:44 +0000

    Added default email to settings

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.conf import settings
+
 from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
@@ -18,6 +20,26 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """
+        Send the user a confirmation email
+        """
+        cust_email = order.email
+        print(cust_email)
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """
@@ -92,6 +114,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             # if order, return 200 response
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS:\
@@ -132,8 +155,6 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        # if the order has been created by the webhook handler
-        # the email is sent after the order was created
         self._send_confirmation_email(order)
         # returns status 200 response
         return HttpResponse(
