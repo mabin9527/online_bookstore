f46d6c1acb6b2905e2e3a7d9ba6c18d300b03d2d
mabin9527 <ma.bin9527000@gmail.com>
2023-08-06 07:58:45 +0000

    Added views, urls and editor index templates shell to editor app

--- editor/views.py
+++ editor/views.py
@@ -1,14 +1,27 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import stripe
+import json
+from checkout.models import Order
+
+
 
 @login_required
 def editor_index(request):
     """"
-    A view to display editor homepage
+    A view to display editor homepage, containing order list from customers
     """
-    userinfo = request.user.username
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    orders = Order.objects.all()
+    for order in orders:
+        payment_intent = stripe.PaymentIntent.retrieve(order.stripe_pid)
+
+    userinfo = request.user
     context = {
-        'userinfo': userinfo
+        'userinfo': userinfo,
+        'orders': orders,
+        'payment_intent': payment_intent
     }
     return render(request, 'editor/editor_index.html', context)
 
