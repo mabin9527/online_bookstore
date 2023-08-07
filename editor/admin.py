9ad6853ed5b0145471963edcdca2c666398279dc
mabin9527 <ma.bin9527000@gmail.com>
2023-08-07 09:16:31 +0000

    Registered the staffinfoadmin and test it

--- editor/admin.py
+++ editor/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import StaffInfo
 
-# Register your models here.
+
+class StaffInfoAdmin(admin.ModelAdmin):
+    fields = (
+        'name', 'age', 'create_time', 'depart', 'gender', 
+        'mobile_number', 'street_address1', 'address2', 
+        'city', 'county',
+    )
+    list_display = (
+        'name', 'age', 'create_time', 'depart', 'gender', 
+        'mobile_number', 'street_address1', 'address2', 
+        'city', 'county',
+    )
+    ordering = ('-create_time',)
+
+
+admin.site.register(StaffInfo, StaffInfoAdmin)
